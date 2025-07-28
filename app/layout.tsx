8e7bde884505a041733ec6a897577cf4638d5a1e
Lalith Sai Reddy <your-email@example.com>
2025-07-28 14:08:09 +0530

    Deploy 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,20 @@
 // app/layout.tsx
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs"; // ✅ import ClerkProvider
+import { Providers } from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Hairss Coaction",
+  title: "Hair Coaction",
   description: "Landing page",
 };
-
-
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-          {children}
-        </ClerkProvider>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
